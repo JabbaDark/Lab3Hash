c8ef4f7a8e513063158b2d5bb1e074eb3bcf8af5
JabbaDark <66027234+JabbaDark@users.noreply.github.com>
2020-06-01 13:04:57 +0700

    Add files via upload

---
+++ Lab3Hash/Hash/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Hash
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.HashBox = new System.Windows.Forms.TextBox();
+            this.Seed = new System.Windows.Forms.TextBox();
+            this.RandNum = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.RandBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(191, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Рассчитать хэш";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(335, 24);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(282, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Случайная последовательность";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // HashBox
+            // 
+            this.HashBox.Location = new System.Drawing.Point(29, 54);
+            this.HashBox.Multiline = true;
+            this.HashBox.Name = "HashBox";
+            this.HashBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.HashBox.Size = new System.Drawing.Size(264, 45);
+            this.HashBox.TabIndex = 2;
+            // 
+            // Seed
+            // 
+            this.Seed.Location = new System.Drawing.Point(335, 69);
+            this.Seed.Name = "Seed";
+            this.Seed.Size = new System.Drawing.Size(131, 20);
+            this.Seed.TabIndex = 3;
+            // 
+            // RandNum
+            // 
+            this.RandNum.Location = new System.Drawing.Point(482, 69);
+            this.RandNum.Name = "RandNum";
+            this.RandNum.Size = new System.Drawing.Size(135, 20);
+            this.RandNum.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(335, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Сид";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(482, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Кол-во чисел";
+            // 
+            // RandBox
+            // 
+            this.RandBox.FormattingEnabled = true;
+            this.RandBox.Location = new System.Drawing.Point(335, 96);
+            this.RandBox.Name = "RandBox";
+            this.RandBox.Size = new System.Drawing.Size(282, 121);
+            this.RandBox.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 228);
+            this.Controls.Add(this.RandBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RandNum);
+            this.Controls.Add(this.Seed);
+            this.Controls.Add(this.HashBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Хэш функция";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox HashBox;
+        private System.Windows.Forms.TextBox Seed;
+        private System.Windows.Forms.TextBox RandNum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox RandBox;
+    }
+}
+
---
+++ Lab3Hash/Hash/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Hash
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)// рассчет хэша для текста из файла
+        {
+            using (SHA256 sha256Hash = SHA256.Create())// создаем экземпляр SHA256
+            {
+                var bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(File.ReadAllText("C:\\Code\\Hash.txt")));// рассчитываем хэш для текста из файла в формате UTF8
+                var Hash = new StringBuilder();// StringBuilder используется для формирования хэша в одну строку (есть еще вариант построкового вывода в, например, ListBox, если через .Item.Add)
+                for (int i = 0; i < bytes.Length; i++)// собственно, сама запись хэша в строку
+                {
+                    Hash.Append(bytes[i].ToString("x2"));// x2 - спецификатор точности
+                }
+                HashBox.Text = Hash.ToString();// вывод полученной строки в текстовое окно HashBox
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)// рассчет псевдослучайных последовательностей
+        {
+            RandBox.Items.Clear();
+            using (SHA256 sha256Hash = SHA256.Create())// создаем экземпляр SHA256
+            {
+                for (ulong i = ulong.Parse(Seed.Text); i < ulong.Parse(Seed.Text) + ulong.Parse(RandNum.Text); i++)// из Seed.Text берется сид - начальное значение параметра i, из RandNum.Text - количество случайных последовательностей
+                {
+                    var bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(i.ToString()));// рассчитываем хэш для i в формате UTF8
+                    RandBox.Items.Add(BitConverter.ToUInt64(bytes.Take(8).ToArray(), 0).ToString("x8"));// Вывод в ListBox RandBox с спецификатором точности x8
+                }
+            }
+        }
+    }
+}
